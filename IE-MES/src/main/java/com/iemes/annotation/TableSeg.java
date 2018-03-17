b76fef5976332321375614f94c03930b35ffd9df
leon <nuoke2007@126.com>
2018-03-17 11:10:55 +0800

    test3

--- IE-MES/src/main/java/com/iemes/annotation/TableSeg.java
+++ IE-MES/src/main/java/com/iemes/annotation/TableSeg.java
@@ -8,7 +8,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 需要给两个值<br>
+ * 需要给两个值<br>  
  * tableName = "表名"<br>
  * id = "表的主键"(如果是多个主建,默认为第一个)<br>
  * @author lanyuan 2015-09-02
